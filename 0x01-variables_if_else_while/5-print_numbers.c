f19f371a1f587e60433415ca18a45e7fccc0467c
Timothy Olaniran <mgeek@DESKTOP-0DJ5A92.localdomain>
2022-10-15 05:38:28 +0100

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the digits in base 10
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i == 0; i <= 9; i++)
+		putchar(i);
+	putchar("\n");
+
+	return (0);
+
+}
