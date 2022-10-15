eacb42a8f3421aa99549133667f4c060dd879a45
Timothy Olaniran <mgeek@DESKTOP-0DJ5A92.localdomain>
2022-10-15 05:18:32 +0100

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,16 +3,14 @@
 /**
  * main - prints the alphabet in lowercase,
  * followed by a new line
- * Return:  0 
+ * Return:  0
  */
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
 		putchar(letter);
-	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
