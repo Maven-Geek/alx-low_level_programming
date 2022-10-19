6e95174bed62ef90a5421cc8f7ddaedb30d948ce
Timothy Olaniran <mgeek@DESKTOP-0DJ5A92.localdomain>
2022-10-19 04:43:06 +0100

    update task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,36 +1,53 @@
 #include "main.h"
 
+
+
+
 /**
- * times_table - prints the 9 times table, starting with 0
+ * add - adds two integers
+ * @n1: integer 1
+ * @n2: integer 2
+ *
+ * Description: give two integers, returns sum
+ *
+ * Return: sum
  */
-void times_table(void)
+
+int add(int n1, int n2)
 {
-	int i, j, k;
+	int sum;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
+	sum = n1 + n2;
 
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+	return (sum);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
