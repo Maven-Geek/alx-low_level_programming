babc05e49e0ca080e5080eda63ac6740194d6618
Ogooluwa Olaniran <38787794+Maven-Geek@users.noreply.github.com>
2022-10-24 22:33:19 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * swap_int: swaps numbers a and b
- * @a: pointer to first integer
- * @b: pointer to second integer
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first value
+ * @b: pointer to second value
  */
-
 void swap_int(int *a, int *b)
 {
-	int temp = *a;
+	int c;
+
+	c = *a;
 	*a = *b;
-	*b = temp;
+	*b = c;
 }
